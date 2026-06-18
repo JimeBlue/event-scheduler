b557e21ef0ea25bd79b986915ec00d4f96f3e50d
JimeBlue <jimeblau@gmail.com>
2026-06-18 14:03:46 +0200

    set up React Router with nested event routes and page stubs

---
+++ src/pages/CreateEvent.jsx
@@ -0,0 +1,10 @@
+const CreateEvent = () => {
+  return (
+    <div>
+      <h1 className="font-heading text-4xl">Create Event</h1>
+      <p className="font-text">The create event form will go here.</p>
+    </div>
+  );
+};
+
+export default CreateEvent;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,10 @@
+const SignIn = () => {
+  return (
+    <div>
+      <h1 className="font-heading text-4xl">Sign In</h1>
+      <p className="font-text">The sign in form will go here.</p>
+    </div>
+  );
+};
+
+export default SignIn;
